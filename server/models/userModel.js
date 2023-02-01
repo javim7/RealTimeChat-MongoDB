9c55ef437b7d970c76edfb6f1ea4d475d6551619
javim7 <61723252+javim7@users.noreply.github.com>
2023-02-01 14:27:18 -0600

    Revert "user model update"
    This reverts commit 9a8902f690edd7857fc92b8b642718f52a7e6565.

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,22 +3,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     name: {
         type: String,
         required: true
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
+    age: {
+        type: Number,
         required: true
     }
 }, {timestamps: true})
--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const multer = require('multer');
 
 //routes
 const userRoutes = require('./routes/users');
